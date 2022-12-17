6b56994930dc0476b9f3d7d67999f4dbffab74a6
Mehedi Hasan <114255330+expartmehedi@users.noreply.github.com>
2022-12-18 00:30:17 +0600

    java practices

---
+++ app.js
@@ -0,0 +1,6 @@
+let name = prompt('Your Name Pleace')
+let age = prompt('Your Age Pleace')
+let skill = prompt('Your Skill Pleace')
+
+
+console.log(`Hi ${ name }, you are ${ age } years old and you ${ skill }`);
